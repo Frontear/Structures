6bdabbc4ba8eae8a58f53efe2799e6a84f2e1d9d
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-01-14 15:46:24 -0500

    feat(print): Linked List can now be printed. Construction of it is still rudimentary, but we will improve that

--- src/main.c
+++ src/main.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+void print_list();
+
+Node* head = NULL;
 
 int main(void) {
-    printf("Hello world!\n");
+    Node* a = malloc(sizeof(Node));
+    Node* b = malloc(sizeof(Node));
+    Node* c = malloc(sizeof(Node));
+
+    if (!a || !b || !c) {
+        fprintf(stderr, "Failed to allocate for one or more nodes\n");
+        return -1;
+    }
+
+    a->data = 10;
+    b->data = 13;
+    c->data = 5;
+
+    a->next = b;
+    b->next = c;
+    c->next = NULL;
+
+    head = a;
+
+    print_list();
 
     return 0;
+}
+
+void print_list() {
+    Node* current = head;
+
+    printf("Linked List: ");
+
+    while (current != NULL) {
+        printf("%d", current->data);
+
+        if (current->next != NULL) {
+            printf(" --> ");
+        }
+
+        current = current->next;
+    }
+
+    printf("\n");
 }
